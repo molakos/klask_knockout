c6d62420787f773551a9aec0c345439ebf59f73d
molakos <molak06@gmail.com>
2020-04-03 12:03:01 +0200

    Delete unnecessary code and rename HelloWorld component

--- src/views/Home.vue
+++ src/views/Home.vue
@@ -1,18 +1,17 @@
 <template>
   <div class="home">
-    <img alt="Vue logo" src="../assets/logo.png">
-    <HelloWorld msg="Welcome to Your Vue.js App"/>
+    <HomeNav msg="Welcome"/>
   </div>
 </template>
 
 <script>
 // @ is an alias to /src
-import HelloWorld from '@/components/HelloWorld.vue'
+import HomeNav from '@/components/HomeNav.vue'
 
 export default {
   name: 'Home',
   components: {
-    HelloWorld
+    HomeNav
   }
 }
 </script>
