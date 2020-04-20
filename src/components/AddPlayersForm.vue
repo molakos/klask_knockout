42f3238507753c840655409407b8cd567eb52e09
molakos <molak06@gmail.com>
2020-04-20 16:16:27 +0200

    feat & style: use StlyledInputs and change logic of the player pool size selection

--- src/components/AddPlayersForm.vue
+++ src/components/AddPlayersForm.vue
@@ -1,30 +1,31 @@
 <template>
-  <form @submit="submit">
-    <h3>Add participants</h3>
-    <div v-for="(input, index) in inputs" :key="input.id">
-      <label :for="'player-name' + index">
-        Name of player {{ index + 1 }}
-      </label>
-      <input
-        :id="'player-name' + index"
-        v-model="input.name"
-        autocomplete="off"
-        placeholder="Enter name"
-      />
-      <button type="button" v-on:click="removePlayer(input.id)">
-        Remove
+  <div class="content-box">
+    <form class="content-form" @submit="submit">
+      <h3>Select players amount</h3>
+      <select class="select" v-model="size" @change="setPlayersBySize">
+        <option value="4">4</option>
+        <option value="8">8</option>
+        <option value="16">16</option>
+        <option value="32">32</option>
+      </select>
+      <hr class="styled-hr" />
+      <h3>Add participants</h3>
+      <div
+        class="inputs-wrapper"
+        v-for="(input, index) in inputs"
+        :key="input.id"
+      >
+        <StyledInput
+          v-model="input.name"
+          v-bind:forId="'player-name' + index"
+          v-bind:label="'Player ' + (index + 1)"
+        />
+      </div>
+      <button class="btn" type="submit">
+        Proceed
       </button>
-    </div>
-    <button v-on:click="addPlayer" type="button">
-      Add a player
-    </button>
-    <button v-on:click="extendPlayersList" type="button">
-      Add 4 more
-    </button>
-    <button type="submit">
-      Proceed
-    </button>
-  </form>
+    </form>
+  </div>
 </template>
 
 <script>
@@ -32,47 +33,76 @@ import { mapActions } from "vuex";
 import { player as playerModel } from "../store/model";
 import { v4 as uuidv4 } from "uuid";
 
+import StyledInput from "../common/StyledInput";
+
 export default {
   name: "AddPlayers",
+  components: {
+    StyledInput,
+  },
   methods: {
     ...mapActions(["addParticipants", "createRounds"]),
     submit(e) {
       e.preventDefault();
-
-      if (this.inputs.length % 4 === 0) {
-        this.addParticipants(this.inputs);
-        this.createRounds();
-      } else {
-        window.alert("Number of participants must be dividable with 4!");
-      }
+      this.addParticipants(this.inputs);
+      this.createRounds();
     },
-    addFourPlayers() {
-      let setOfplayers = [];
+    setPlayersBySize(event) {
+      let setOfplayers = [...this.inputs],
+        size = parseInt(event.target.value, 10);
 
-      for (let i = 0; i < 4; i++) {
+      if (this.inputs.length > size) {
+        setOfplayers = this.decreasePlayers(
+          setOfplayers,
+          this.inputs.length - size
+        );
+      } else {
+        setOfplayers = this.increasePlayers(
+          setOfplayers,
+          size - this.inputs.length
+        );
+      }
+      this.inputs = setOfplayers;
+    },
+    increasePlayers(playersList, increaseByNum) {
+      for (let i = 0; i < increaseByNum; i++) {
         let player = { ...playerModel };
         player.id = uuidv4();
-        setOfplayers.push(player);
+        playersList.push(player);
       }
 
-      return setOfplayers;
+      return playersList;
     },
-    extendPlayersList() {
-      this.inputs = [...this.inputs, ...this.addFourPlayers()];
-    },
-    removePlayer(id) {
-      this.inputs = this.inputs.filter((input) => input.id != id);
-    },
-    addPlayer() {
-      this.inputs = [...this.inputs, { ...playerModel, id: uuidv4() }];
+    decreasePlayers(playersList, decreaseByNum) {
+      let indexFrom = playersList.length - decreaseByNum;
+      playersList.splice(indexFrom, decreaseByNum);
+
+      return playersList;
     },
   },
   data() {
     return {
-      inputs: this.addFourPlayers(),
+      size: 4,
+      inputs: this.increasePlayers([], 4),
     };
   },
 };
 </script>
 
-<style scoped></style>
+<style scoped lang="scss">
+.inputs-wrapper {
+  display: flex;
+}
+
+.styled-hr {
+  border: 0;
+  height: 1px;
+  background: $dark-purple;
+  background-image: linear-gradient(to right, #ccc, $dark-purple, #ccc);
+  width: 100%;
+}
+
+::v-deep .styled-input-root {
+  flex: 1;
+}
+</style>
